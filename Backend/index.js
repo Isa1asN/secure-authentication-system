59f5ee52bf8667a2f66891ce19df3361c9404753
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-06-17 12:28:29 +0300

    updated the root

--- Backend/index.js
+++ Backend/index.js
@@ -3,12 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import {authRouter} from "./routes/authRoutes.js"
-import { userRouter } from "./routes/users.js";
 import { profileRouter } from "./routes/profile.js";
-import { courseRouter } from "./routes/progress.js";
-import { wordRouter } from "./routes/wordOfTD.js";
-import {todaysWordRouter} from "./routes/todaysWord.js"
-import {vocabRouter} from "./routes/vocabulary.js"
 dotenv.config();
 
 const app = express();
@@ -18,23 +13,10 @@ app.use(bodyParser.json());
 // Auth related routes
 app.use('/auth', authRouter);
 
-// Users related routes
-app.use('/api', userRouter);
 
 // Profile route
 app.use('/api', profileRouter);
 
-// course progress route
-app.use('/course-progress', courseRouter);
-
-// word of the day route
-app.use('/wordOfTD', wordRouter)
-
-// Today's word route
-app.use('/api', todaysWordRouter)
-
-// Vocabulary route
-app.use('/api', vocabRouter)
 
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URL,{
